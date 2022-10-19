27db5c15a8442c3d1dce1991a07fe63f5d19cc98
Sebas Amaya <sebasamayaxd@hotmail.com>
2022-10-19 12:10:51 -0500

    Initial structure ok

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+'security/ir.model.access.csv',
+{
+    'name': 'Odoo Evaluation',
+    'description': 'Learn and create my first single app.',
+    'version': '1.0',
+    'author': 'Sebastian Amaya',
+    'category': 'Style',
+
+    'depends': ['sale_management'],
+    'data': [
+        'views/'
+    ],
+    'installable': true
+}
