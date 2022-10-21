b56cc63d0bedf7749e72d33ae3a6fb6a93590a90
Sebas Amaya <sebasamayaxd@hotmail.com>
2022-10-21 09:47:53 -0500

    Many2one relation has been added

--- models/models.py
+++ models/models.py
@@ -6,6 +6,7 @@ class SaleOrder(models.Model):
     sale_type = fields.Selection(selection=[("national", "National"), ("international", "International")])
     is_international = fields.Boolean(string='Internacional?')
     operator_type = fields.Selection(selection=[("import", "Import"), ("export", "Export")])
+    product_id = fields.Many2one('producto.template', string='Código Producto')
     reference_type = fields.Char(string='Referencia')
     is_danger = fields.Boolean(string='Peligroso?')
     value = fields.Float(string='Precio')
