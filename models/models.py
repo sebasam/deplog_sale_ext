117d0fd1342d7c390e89e3193dd51b25f12caac3
Sebas Amaya <sebasamayaxd@hotmail.com>
2022-10-21 09:40:27 -0500

    Problem with fields type select fixed in models

--- models/models.py
+++ models/models.py
@@ -1,12 +1,11 @@
 from odoo import models, fields
 
-class Order(models.Model):
-    _name = "sale.order"
+class SaleOrder(models.Model):
+    _name = 'sale.order'
 
-    sale_type = fields.Selection(['National', 'International'], string='Tipo de venta')
+    sale_type = fields.Selection(selection=[("national", "National"), ("international", "International")])
     is_international = fields.Boolean(string='Internacional?')
-    operator_type = fields.Selection(['Import', 'Export'], string='Operation')
-    product_id = fields.Many2One('producto.template', string='Código Producto')
+    operator_type = fields.Selection(selection=[("import", "Import"), ("export", "Export")])
     reference_type = fields.Char(string='Referencia')
     is_danger = fields.Boolean(string='Peligroso?')
     value = fields.Float(string='Precio')
