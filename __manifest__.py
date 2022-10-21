2e740ad4ec5edbb70de260477de5ba12e9b15021
Sebastian Amaya <91269682+sebasam@users.noreply.github.com>
2022-10-21 12:55:17 -0500

    First steps (#1)
    * Initial structure ok
    
    * model order created
    
    * initial form ok, first deploy in process
    
    * changes in files security, it looks like that was bad write
    
    * All files deleted, app prototype had  many bugs, therefore it will be reinitialized
    
    * sale.order model ok
    
    * Problem with fields type select fixed in models
    
    * Many2one relation has been added
    
    * file with views can be installed in odoo
    
    * module can be installed with own views and controllers empty

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name":"deplog_sale_ext",
+    "description":"App model sale.order in odoo",
+    "data": [
+        "views/sale_order.xml",
+        "security/security.xml"
+    ],
+    "depends": [
+        "sale_management"
+    ]
+}
---
+++ models/sale_order.py
@@ -0,0 +1,12 @@
+from odoo import models, fields
+
+class SaleOrder(models.Model):
+    _name = 'sales.order'
+
+    sale_type = fields.Selection(selection=[("national", "National"), ("international", "International")])
+    is_international = fields.Boolean(string='Internacional?')
+    operator_type = fields.Selection(selection=[("import", "Import"), ("export", "Export")])
+    product_id = fields.Many2one('producto.template', string='Código Producto')
+    reference_type = fields.Char(string='Referencia')
+    is_danger = fields.Boolean(string='Peligroso?')
+    value = fields.Float(string='Precio')
